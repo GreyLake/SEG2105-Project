82a8d411d656d67659df973515c39cbd75ad6d00
BuildTools <unconfigured@null.spigotmc.org>
2021-09-13 21:01:23 -0400

    Project

---
+++ app/src/main/java/com/SEG2105/project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.SEG2105.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
